315a0a98bb4192128f9d01ce20ac8baeee15d8c1
ch4k4uw <garra_e@hotmail.com>
2022-03-23 17:16:28 -0300

    Fixed unit test to fit with LiveData to Flow<> migration

--- app/src/test/java/com/sicredi/instacredi/feed/stuff/FeedViewModelLogoutTestCases.kt
+++ app/src/test/java/com/sicredi/instacredi/feed/stuff/FeedViewModelLogoutTestCases.kt
@@ -1,8 +1,7 @@
 package com.sicredi.instacredi.feed.stuff
 
-import com.sicredi.instacredi.feed.interaction.FeedState
+import com.sicredi.presenter.feed.interaction.FeedState
 import io.mockk.coVerify
-import io.mockk.verify
 
 class FeedViewModelLogoutTestCases(
     container: FeedViewModelTestContainer
@@ -16,7 +15,7 @@ class FeedViewModelLogoutTestCases(
 
         coVerify(exactly = 0) { container.findAllEvents.invoke() }
         coVerify(exactly = 1) { container.performLogout.invoke() }
-        verify(exactly = 1) { container.viewModelObserver.onChanged(FeedState.Loading) }
-        verify(exactly = 1) { container.viewModelObserver.onChanged(FeedState.SuccessfulLoggedOut) }
+        coVerify(exactly = 1) { container.viewModelObserver.emit(FeedState.Loading) }
+        coVerify(exactly = 1) { container.viewModelObserver.emit(FeedState.SuccessfulLoggedOut) }
     }
 }
--- app/src/test/java/com/sicredi/instacredi/feed/stuff/FeedViewModelTestContainer.kt
+++ app/src/test/java/com/sicredi/instacredi/feed/stuff/FeedViewModelTestContainer.kt
@@ -3,16 +3,17 @@ package com.sicredi.instacredi.feed.stuff
 import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateHandle
 import com.sicredi.instacredi.AppInstantTaskExecutorRule
-import com.sicredi.instacredi.common.uc.FindEventDetails
-import com.sicredi.instacredi.common.uc.PerformLogout
-import com.sicredi.instacredi.feed.interaction.FeedState
-import com.sicredi.instacredi.feed.uc.FindAllEvents
+import com.sicredi.presenter.common.uc.FindEventDetails
+import com.sicredi.presenter.common.uc.PerformLogout
+import com.sicredi.presenter.feed.interaction.FeedState
+import com.sicredi.presenter.feed.uc.FindAllEvents
+import kotlinx.coroutines.flow.FlowCollector
 
 data class FeedViewModelTestContainer(
     val savedStateHandle: SavedStateHandle,
     val findAllEvents: FindAllEvents,
     val findEventDetails: FindEventDetails,
     val performLogout: PerformLogout,
-    val viewModelObserver: Observer<FeedState>,
+    val viewModelObserver: FlowCollector<FeedState>,
     val testRule: AppInstantTaskExecutorRule
 )
