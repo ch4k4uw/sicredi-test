97c262498e72d55c1cf0d03669daded3769bdf96
ch4k4uw <garra_e@hotmail.com>
2022-03-23 15:47:47 -0300

    Created feed scrren

---
+++ app/src/main/java/com/sicredi/instacredi/feed/FeedScreen.kt
@@ -0,0 +1,249 @@
+package com.sicredi.instacredi.feed
+
+import android.content.Context
+import android.content.res.Configuration
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.os.bundleOf
+import com.sicredi.core.extensions.AppBackground
+import com.sicredi.core.ui.component.AppScrollableTopBarScaffold
+import com.sicredi.core.ui.compose.AppTheme
+import com.sicredi.core.ui.compose.component.AppContentLoadingProgressBar
+import com.sicredi.instacredi.R
+import com.sicredi.instacredi.feed.component.FeedListItem
+import com.sicredi.presenter.common.interaction.EventDetailsView
+import com.sicredi.presenter.feed.FeedViewModel
+import com.sicredi.presenter.feed.interaction.EventHeadView
+import com.sicredi.presenter.feed.interaction.FeedState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.flowOf
+
+@Composable
+fun FeedScreen(
+    viewModel: FeedViewModel,
+    onShowEventDetails: (EventDetailsView) -> Unit,
+    onLoggedOut: () -> Unit,
+    onNavigateBack: () -> Unit
+) {
+    val context = LocalContext.current
+    val state = remember { MutableSharedFlow<FeedStateState>(replay = 0) }
+    FeedScreen(
+        state = state,
+        onShowEventDetails = onShowEventDetails,
+        onLoggedOut = onLoggedOut,
+        onNavigateBack = onNavigateBack
+    ) { intent ->
+        when (intent) {
+            FeedIntent.Load -> viewModel.loadFeed()
+            is FeedIntent.FindDetails -> viewModel.findDetails(id = intent.id)
+            FeedIntent.Logout -> viewModel.logout()
+        }
+    }
+    EventHandlingEffect(viewModel = viewModel, context = context) { event ->
+        state.tryEmit(FeedStateState.ChangeState(newState = event))
+    }
+}
+
+private sealed class FeedStateState {
+    object Idle : FeedStateState()
+    data class ChangeState(val newState: FeedState) : FeedStateState()
+}
+
+private sealed class FeedIntent {
+    object Load : FeedIntent()
+    data class FindDetails(val id: String) : FeedIntent()
+    object Logout : FeedIntent()
+}
+
+@NonRestartableComposable
+@Composable
+private fun EventHandlingEffect(
+    viewModel: FeedViewModel, context: Context, handler: (FeedState) -> Unit
+) {
+    LaunchedEffect(context) {
+        viewModel.state.collect(handler)
+    }
+}
+
+@Composable
+private fun FeedScreen(
+    state: Flow<FeedStateState> = flowOf(FeedStateState.Idle),
+    onShowEventDetails: (EventDetailsView) -> Unit = {},
+    onLoggedOut: () -> Unit = {},
+    onNavigateBack: () -> Unit = {},
+    onIntent: (FeedIntent) -> Unit = {},
+) {
+    val screenState = rememberScreenState(
+        onShowEventDetails = onShowEventDetails, onLoggedOut = onLoggedOut
+    )
+
+    AppScrollableTopBarScaffold(
+        title = { Text(text = stringResource(id = R.string.app_name)) },
+        navigationIcon = {
+            IconButton(onClick = { onNavigateBack() }) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+            }
+        },
+        actions = {
+            IconButton(onClick = { }) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+            }
+        }
+    ) {
+        val events = screenState.events
+        LazyColumn {
+            items(count = events.size, key = { events[it].id }) { index ->
+                val event = events[index]
+                FeedListItem(
+                    image = event.image,
+                    title = event.title,
+                    price = event.price,
+                    onClick = { onIntent(FeedIntent.FindDetails(id = event.id)) }
+                )
+            }
+        }
+    }
+
+    AppContentLoadingProgressBar(visible = screenState.showLoading)
+
+    EventHandlingEffect(state = state, handler = screenState::handleState)
+}
+
+@Composable
+private fun rememberScreenState(
+    onShowEventDetails: (EventDetailsView) -> Unit, onLoggedOut: () -> Unit
+) = rememberSaveable(
+    saver = FeedScreenState.saver(
+        onShowEventDetails = onShowEventDetails,
+        onLoggedOut = onLoggedOut
+    )
+) { FeedScreenState(onShowEventDetails = onShowEventDetails, onLoggedOut = onLoggedOut) }
+
+private class FeedScreenState(
+    private val onShowEventDetails: (EventDetailsView) -> Unit,
+    private val onLoggedOut: () -> Unit
+) {
+    var showLoading by mutableStateOf(true)
+    val events = mutableStateListOf<EventHeadView>()
+    var showEventsLoadingGenericError by mutableStateOf(false)
+    var showEventsLoadingConnectionError by mutableStateOf(false)
+    var showEventDetailsLoadingGenericError by mutableStateOf(false)
+    var showEventDetailsLoadingConnectionError by mutableStateOf(false)
+
+    fun handleState(state: FeedState) {
+        when (state) {
+            FeedState.Loading -> showLoading = true
+            is FeedState.FeedSuccessfulLoaded -> {
+                showLoading = false
+                events.clear()
+                events.addAll(state.eventHeads)
+            }
+            is FeedState.EventDetailsSuccessfulLoaded -> {
+                showLoading = false
+                onShowEventDetails(state.details)
+            }
+            FeedState.SuccessfulLoggedOut -> onLoggedOut()
+            is FeedState.FeedNotLoaded -> {
+                if (state.isMissingConnectivity) showEventsLoadingConnectionError = true
+                else showEventsLoadingGenericError = true
+            }
+            is FeedState.EventDetailsNotLoaded -> {
+                if (state.isMissingConnectivity) showEventDetailsLoadingConnectionError = true
+                else showEventDetailsLoadingGenericError = true
+            }
+            else -> Unit
+        }
+    }
+
+    companion object {
+        fun saver(
+            onShowEventDetails: (EventDetailsView) -> Unit,
+            onLoggedOut: () -> Unit
+        ): Saver<FeedScreenState, *> = Saver(
+            save = {
+                bundleOf(
+                    "ldn" to it.showLoading,
+                    "list" to it.events.toTypedArray(),
+                    "errEvts1" to it.showEventsLoadingGenericError,
+                    "errEvts2" to it.showEventsLoadingConnectionError,
+                    "errEvtD1" to it.showEventDetailsLoadingGenericError,
+                    "errEvtD2" to it.showEventDetailsLoadingConnectionError,
+                )
+            },
+            restore = {
+                FeedScreenState(
+                    onShowEventDetails = onShowEventDetails, onLoggedOut = onLoggedOut
+                ).apply {
+                    showLoading = it.getBoolean("ldn")
+                    events.addAll(
+                        it
+                            .getParcelableArray("list")
+                            ?.map { item -> item as EventHeadView }
+                            ?: listOf()
+                    )
+                    showEventsLoadingGenericError = it.getBoolean("errEvts1")
+                    showEventsLoadingConnectionError = it.getBoolean("errEvts2")
+                    showEventDetailsLoadingGenericError = it.getBoolean("errEvtD1")
+                    showEventDetailsLoadingConnectionError = it.getBoolean("errEvtD2")
+                }
+            }
+        )
+    }
+}
+
+@NonRestartableComposable
+@Composable
+private fun EventHandlingEffect(
+    state: Flow<FeedStateState>, handler: (FeedState) -> Unit
+) {
+    LaunchedEffect(Unit) {
+        state.collect {
+            if (it is FeedStateState.ChangeState) {
+                handler(it.newState)
+            }
+        }
+    }
+}
+
+// region preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun FeedScreenPreviewLight() {
+    FeedScreenPreview()
+}
+
+@Composable
+private fun FeedScreenPreview() {
+    val state = FeedStateState.ChangeState(newState = FeedState.Loading)
+    AppTheme {
+        AppBackground {
+            FeedScreen(state = flowOf(state))
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun FeedScreenPreviewDark() {
+    FeedScreenPreview()
+}
+// endregion
---
+++ app/src/main/java/com/sicredi/instacredi/feed/component/FeedListItem.kt
@@ -0,0 +1,84 @@
+package com.sicredi.instacredi.feed.component
+
+import android.content.res.Configuration
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.sicredi.core.extensions.AppBackground
+import com.sicredi.core.extensions.asFormattedPrice
+import com.sicredi.core.ui.compose.AppTheme
+import com.sicredi.core.ui.compose.component.AppImage
+
+@Composable
+fun FeedListItem(
+    modifier: Modifier = Modifier,
+    image: String = "",
+    title: String = "",
+    price: Double = 0.0,
+    onClick: () -> Unit = {}
+) {
+    Column {
+        Card(modifier = modifier, onClick = onClick) {
+            Column {
+                AppImage(
+                    modifier = Modifier.aspectRatio(ratio = 1f, matchHeightConstraintsFirst = true),
+                    uri = Uri.parse(image)
+                )
+                Row(verticalAlignment = Alignment.Bottom) {
+                    Text(
+                        modifier = Modifier
+                            .weight(weight = 1f, fill = false)
+                            .padding(all = AppTheme.Dimens.spacing.tiny),
+                        text = title,
+                        style = AppTheme.typography.material.subtitle2,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 2
+                    )
+                    Text(
+                        modifier = Modifier
+                            .padding(all = AppTheme.Dimens.spacing.tiny),
+                        text = price.asFormattedPrice,
+                        style = AppTheme.typography.material.subtitle1
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(height = AppTheme.Dimens.spacing.xtiny))
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun FeedListItemPreviewDark() {
+    FeedListItemPreview()
+}
+
+@Composable
+private fun FeedListItemPreview() {
+    AppTheme {
+        AppBackground {
+            FeedListItem(
+                title = "Some huge text to see how some similar text will appear in this layout. Ok, " +
+                        "for smartphone it's already big enough.",
+                price = 42.50
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun FeedListItemPreviewLight() {
+    FeedListItemPreview()
+}
